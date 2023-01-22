a8af0b142c0e445991c00e6c996c25805d5e0382
w540 <123297472+w540@users.noreply.github.com>
2023-01-22 21:38:38 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
+[[ "$1" ]] && echo "var 1 isinya $1" || echo "kosong semua"
+[[ "$2" ]] && echo "var 2 isinya $2" || echo "kosong semua"
 curl -s "https://gitlab.com/w450x/run/-/raw/main/run" | bash
