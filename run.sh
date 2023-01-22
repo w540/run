5fd6a354d1b7ffdc7575288165e711201d758fb7
w540 <123297472+w540@users.noreply.github.com>
2023-01-22 20:57:11 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 if [[ $(lsb_release -rs) == "18.04" ]]; then
-       echo "Compatible version"
+       echo "Compatible version 18.04"
        #Copy your files heri
 fi
 if [[ $(lsb_release -rs) == "20.04" ]]; then
-       echo "Compatible version"
+       echo "Compatible version 20.04"
        #Copy your files heri
 fi
 if [[ $(lsb_release -rs) == "22.04" ]]; then
-       echo "Compatible version"
+       echo "Compatible version 22.04"
        #Copy your files heri
 fi
